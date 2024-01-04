7c763c0b1de2ca039e44f29a93b85d28fce7d0cf
RaximboyIbroximov <raximboy4200@gmail.com>
2024-01-04 11:12:53 +0500

    2-version

--- test/main.py
+++ test/main.py
@@ -9,6 +9,10 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+print('salom dunyo')
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
